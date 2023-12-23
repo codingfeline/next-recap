3ee92d7bf7fc8402c266deed36117069849cf808
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-12-23 12:17:43 +0000

    init

---
+++ app/NavBar.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <ul className="flex gap-1">
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/users">Users</Link>
+      </li>
+    </ul>
+  )
+}
+
+export default NavBar
