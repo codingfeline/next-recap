303c8529383a4a215180fb9416d1a4ba9e55a6c8
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-12-30 07:46:42 +0000

    add lazy lodash

--- app/other/page.tsx
+++ app/other/page.tsx
@@ -3,12 +3,15 @@
 import dynamic from 'next/dynamic'
 import React, { useState } from 'react'
 // import BigComponent from '../components/BigComponent'
+// import _ from 'lodash'
 
 const Heavy = dynamic(() => import('@/app/components/BigComponent'), {
+  ssr: false, // * to disable pre-rendering on the server if accessing browser APIs on the server
   loading: () => <p>Loading...</p>,
 })
 
 const OtherPage = () => {
+  const [users, setUsers] = useState([{ name: 'c' }, { name: 'b' }, { name: 'a' }])
   const [isVisible, setVisible] = useState(false)
 
   return (
@@ -16,6 +19,20 @@ const OtherPage = () => {
       <h2>Other page</h2>
       <button onClick={() => setVisible(true)}>Show Big Component</button>
       {isVisible && <Heavy />}
+
+      <hr />
+      <button
+        onClick={async () => {
+          const _ = (await import('lodash')).default
+          // const _ = await import('lodash')
+          const sorted = setUsers(_.orderBy(users, ['name']))
+          console.log(sorted)
+        }}
+      >
+        Sort
+      </button>
+      <hr />
+      {JSON.stringify(users)}
     </div>
   )
 }
